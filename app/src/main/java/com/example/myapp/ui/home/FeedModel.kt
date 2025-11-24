a61fe43d6486e6f547893badeb43f8a1dbf80472
sunnydome <2396461097@qq.com>
2025-11-24 17:27:28 +0800

    feat(init) Add Application class to init app and database

--- app/src/main/java/com/example/myapp/ui/home/FeedModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapp.ui.home
-
-data class FeedModel (
-    val id: String,
-    val imageUrl: String,
-    val description: String,
-    val userName: String,
-    val userAvatar: String
-)
--- app/src/main/java/com/example/myapp/ui/post/CommentModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapp.ui.post
-
-data class CommentModel (
-    val id: String,
-    val time: String,
-    val content: String,
-    val userName: String,
-    val userAvatar: String
-)
